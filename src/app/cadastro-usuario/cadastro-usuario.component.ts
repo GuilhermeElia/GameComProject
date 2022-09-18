8d0b95884f7c3454b0005b62df1f512a405ed202
Thiago Gomes <thiago.gomes@activesoftconsultoria.com.br>
2022-09-17 21:21:39 -0300

    Criação da tela de Login

--- src/app/cadastro-usuario/cadastro-usuario.component.ts
+++ src/app/cadastro-usuario/cadastro-usuario.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+import {Router} from "@angular/router"
 
 @Component({
   selector: 'app-cadastro-usuario',
   templateUrl: './cadastro-usuario.component.html',
-  styleUrls: ['./cadastro-usuario.component.css']
+  styleUrls: ['./cadastro-usuario.component.css',
+              './css/main.css',
+              './css/util.css',
+              './fonts/font-awesome-4.7.0/css/font-awesome.min.css',
+              './vendor/animate/animate.css',
+              './vendor/bootstrap/css/bootstrap.min.css',
+              './vendor/css-hamburgers/hamburgers.min.css',
+              './vendor/select2/select2.min.css',
+            ]
 })
 export class CadastroUsuarioComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
+
+  pathImgLogin: string = 'assets/img-01.png'
 
   ngOnInit(): void {
+  }
+
+  onSubmit(data: NgForm): void {
+    const values = data.value
+    if (values.login === 'gamecom' && values.pass === '123') {
+      this.router.navigate(['/home'])
+    } else {
+      alert('Login ou senha Incorreto')
+    }
+
   }
 
 }
